991247863585718d9fec349ca09dc84c239e2646
Avnindra Singh <cricketeravnindra1234@gmail.com>
2024-09-10 18:23:23 +0530

    connected database

--- backened/server.js
+++ backened/server.js
@@ -5,9 +5,9 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase= require("./config/database")
 
+// connected database
 dotenv.config({path:"backened/config/config.env"});
 connectDatabase()
-// comment
 
 app.listen(process.env.PORT,()=>{
     console.log('server is working on http://localhost:${process.env.PORT}')
